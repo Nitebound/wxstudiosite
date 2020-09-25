2d71ad00aae8015b77a7b3a0a4b3866683b6ccf9
Zachery <boglov@github.com>
2020-09-24 22:45:43 -0700

    news/variables update

--- page-handler.js
+++ page-handler.js
@@ -1,6 +1,12 @@
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function topFunctionOld() { 
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+function topFunction() {
+  cframe = document.getElementById('content-frame');
+  window.scrollTo(0, 0); 
+  cframe.scrollTo(0, 0);
+}
