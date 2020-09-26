d2e4cccd03f83e835142e3976e4490dcf5cb6e5f
Zachery <boglov@github.com>
2020-09-26 16:04:06 -0700

    variables update

--- media/tutorialdata/helloworld.py
+++ media/tutorialdata/helloworld.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+contacts_dict = {
+    "james":"555 555 1212",
+    "steve":"555 552 3213",
+    "sophia":"555 555 9874",
+    "zach":"555 554 0877"
+}
+
+print("Contacts:", contacts_dict)
