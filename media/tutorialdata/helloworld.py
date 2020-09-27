4cc8b5aa502f26aacbb98d16ba9e1ddfcde70365
Zachery <boglov@github.com>
2020-09-27 02:27:52 -0700

    download/operators update

--- media/tutorialdata/helloworld.py
+++ media/tutorialdata/helloworld.py
@@ -1,2 +1,19 @@
-my_phrase = 'Hello, World!'
-print(my_phrase)
+n1 = 12
+n2 = 5
+n3 = 80
+
+# Addition
+result = n1 + n2
+print("%s + %s ="%(n1, n2), result)
+
+# Subtraction
+result = n1 - n2
+print("%s - %s ="%(n1, n2), result)
+
+# Multiplication
+result = n1 * n2
+print("%s * %s ="%(n1, n2), result)
+
+# Division
+result = n1 / n2
+print("%s / %s ="%(n1, n2), result)
