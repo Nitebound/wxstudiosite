20d7a3127e5cbe45d57060a1e76c4aebc75f4233
Zachery <boglov@github.com>
2020-09-26 23:39:15 -0700

    helloworld update

--- media/tutorialdata/helloworld.py
+++ media/tutorialdata/helloworld.py
@@ -1,8 +1,2 @@
-contacts_dict = {
-    "james":"555 555 1212",
-    "steve":"555 552 3213",
-    "sophia":"555 555 9874",
-    "zach":"555 554 0877"
-}
-
-print("Contacts:", contacts_dict)
+my_phrase = 'Hello, World!'
+print(my_phrase)
